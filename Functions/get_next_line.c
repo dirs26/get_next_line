fd791ee268da348d8b47578fd98eeaab896793c1
Rodrigues <dirs26@gmail.com>
2024-11-28 15:05:45 +0100

    adding functions to help the gnl (get next line) work properly, such as: print_newline_helper, append_buffer, get_line, get remaining).

--- Functions/get_next_line.c
+++ Functions/get_next_line.c
@@ -10,29 +10,113 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdio.h>
 # include "get_next_line.h"
 
-char  *read_from_file(int fd)
+void    print_newline_helper(char *buffer)
 {
-	int  bytes_read;
-	char *cup_buffer;
-	static int count = 1;
-   
-	printf("ft_calloc#[%d]---", count++);
-   	cup_buffer = ft_calloc (3 + 1, sizeof(char));
-   	if (cup_buffer == NULL)
-		return (NULL);
-	bytes_read = read(fd, cup_buffer, 3);
-	if (bytes_read <= 0)
-		return (free (cup_buffer), NULL);
-  return (cup_buffer);
+
+    while (*buffer &&  *buffer != '\0')
+    {
+        if (*buffer == '\n') 
+        {
+            *buffer= '\\';
+        }
+        printf("%c",*buffer);
+        buffer++;
+    }
+}
+
+char *append_buffer(char *basin_buffer, char *read_buffer)
+{
+ char *temp;
+
+ temp = ft_strjoin(basin_buffer, read_buffer);
+ free(basin_buffer);
+ return (temp);
+}
+
+char *read_from_file(char *basin_buffer, int fd)
+{
+    char *cup_buffer;
+    int bytes_read;
+
+    cup_buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    if (!cup_buffer)
+        return (NULL);
+
+    bytes_read = 1;
+    while (bytes_read > 0)
+    {
+        bytes_read = read(fd, cup_buffer, BUFFER_SIZE);
+        if (bytes_read == -1)
+        {
+            free(cup_buffer);
+            return (NULL);
+        }
+
+        if (bytes_read == 0)
+        {
+            free(cup_buffer);
+            return (basin_buffer);
+        }
+
+        cup_buffer[bytes_read] = '\0';
+        basin_buffer = append_buffer(basin_buffer, cup_buffer);
+
+        if (ft_strchr(basin_buffer, '\n'))
+            break;
+    }
+
+    free(cup_buffer);
+    return (basin_buffer);
 }
 
 char *get_next_line(int fd)
 {
- char *basin_buffer;
+    static char *basin_buffer;
+    char *line;
 
- basin_buffer = read_from_file(fd);
- return (basin_buffer);
+    if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+
+    if (!basin_buffer)
+        basin_buffer = ft_calloc(1, sizeof(char));
+    if (!ft_strchr(basin_buffer, '\n'))
+        basin_buffer = read_from_file(basin_buffer, fd);
+
+    if (!basin_buffer || basin_buffer[0] == '\0')
+        return (NULL);
+    line = get_line(basin_buffer);
+    basin_buffer = get_remaining(basin_buffer);
+
+    return (line);
+}
+
+char *get_line(char *basin_buffer)
+{
+    char *line;
+    int i;
+
+    i = 0;
+    while (basin_buffer[i] && basin_buffer[i] != '\n')
+        i++;
+    if (basin_buffer[i] == '\n')
+        i++;
+    line = strndup(basin_buffer, i);
+    return (line);
+}
+
+char *get_remaining(char *basin_buffer)
+{
+    char *remaining;
+    int i;
+
+    i = 0;
+    while (basin_buffer[i] && basin_buffer[i] != '\n')
+        i++;
+    if (basin_buffer[i] == '\n')
+        i++;
+    remaining = strdup(&basin_buffer[i]);
+    free(basin_buffer); 
+    return (remaining);
 }
--- Functions/get_next_line.h
+++ Functions/get_next_line.h
@@ -17,10 +17,12 @@
 # define BUFFER_SIZE 3
 # endif
 
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stddef.h>
+# include <unistd.h>
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlen(const char *str);
@@ -31,5 +33,11 @@ void		ft_bzero(void *s, int n);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 unsigned int		ft_strlcat(char *dst, const char *src, unsigned int dstsize);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void    print_newline_helper(char *buffer);
+char *append_buffer(char *basin_buffer, char *read_buffer);
+char *read_from_file(char *basin_buffer, int fd);
+char *get_next_line(int fd);
+char *extract_line(char *basin_buffer);
+char *obtain_remaining(char *basin_buffer);
 
 #endif
