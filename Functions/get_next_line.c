42258756d4acf40bc6e2bfad982abbcd7d7bb211
Rodrigues <dirs26@gmail.com>
2024-11-28 12:08:11 +0100

    make some changes in get_next_line.c (separating functions, one to read data, and other to get the next line)

--- Functions/get_next_line.c
+++ Functions/get_next_line.c
@@ -10,18 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <stdio.h>
 # include "get_next_line.h"
 
-char  *get_next_line(int fd)
+char  *read_from_file(int fd)
 {
-  int  bytes_read;
-  char *cup_buffer;
-  
-  cup_buffer = ft_calloc (3 + 1, sizeof(char));
-  if (!cup_buffer)
-    return (NULL);
-  bytes_read = read(fd, cup_buffer, 3);
-  if (bytes_read <= 0)
-    return (NULL);
+	int  bytes_read;
+	char *cup_buffer;
+	static int count = 1;
+   
+	printf("ft_calloc#[%d]---", count++);
+   	cup_buffer = ft_calloc (3 + 1, sizeof(char));
+   	if (cup_buffer == NULL)
+		return (NULL);
+	bytes_read = read(fd, cup_buffer, 3);
+	if (bytes_read <= 0)
+		return (free (cup_buffer), NULL);
   return (cup_buffer);
+}
+
+char *get_next_line(int fd)
+{
+ char *basin_buffer;
+
+ basin_buffer = read_from_file(fd);
+ return (basin_buffer);
 }
